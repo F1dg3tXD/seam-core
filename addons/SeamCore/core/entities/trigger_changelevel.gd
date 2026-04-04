4238f5f5737860f27eaf9bdc65846aaf0c116495
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2026-04-04 05:19:00 -0700

    Testing SeamCore

--- addons/SeamCore/core/entities/trigger_changelevel.gd
+++ addons/SeamCore/core/entities/trigger_changelevel.gd
@@ -9,6 +9,6 @@ func _ready():
 	area.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body.name == "PlayerCharacter":
 		var transition: SeamLevelTransition = get_node("/root/SeamLevelTransition")
 		transition.transition_to_scene(target_scene, landmark_name, body, [body])
